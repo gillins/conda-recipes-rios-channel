73a59a0bc7171296ecfa5184763e65230ef6a169
Sam Gillingham <gillingham.sam@gmail.com>
2015-12-22 15:42:58 +1000

    more lastools and pyliadr fixes

--- pylidar/bld.bat
+++ pylidar/bld.bat
@@ -6,5 +6,7 @@ set "RIWAVELIB_ROOT=c:\dev\riwavelib_wfm-2_0_1-x86_64-windows-vc100"
 if not exist %RIVLIB_ROOT% (set "RIVLIB_ROOT=")
 if not exist %RIWAVELIB_ROOT% (set "RIWAVELIB_ROOT=")
 
+set LASTOOLS_ROOT=%LIBRARY_LIB%
+
 %PYTHON% setup.py install
 if errorlevel 1 exit 1
