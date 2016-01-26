12eef362be538e6765a751352290762880569ebb
Sam Gillingham <gillingham.sam@gmail.com>
2016-01-26 19:29:50 +1000

    build lastools utils also. Dll build not possible yet as not all necessary classes exported

--- lastools/bld.bat
+++ lastools/bld.bat
@@ -1,12 +1,13 @@
 
 REM Supplied VC workspaces don't work for me
 REM Martin doesn't have EXPORTS so can't (easily) make this into a DLL
-REM Haven't (yet) got the utils building
 nmake -f makefile.vc
 if errorlevel 1 exit 1
 
 mkdir %LIBRARY_LIB%
 copy laslib.lib %LIBRARY_LIB%
+mkdir %LIBRARY_BIN%
+copy *.exe %LIBRARY_BIN%
 mkdir %LIBRARY_INC%
 copy LASlib\inc\*.* %LIBRARY_INC%
 copy LASzip\src\*.hpp %LIBRARY_INC%
