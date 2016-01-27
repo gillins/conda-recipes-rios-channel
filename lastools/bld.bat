e326032d4f7bc4d788b23687e51f58405165a8c2
Sam Gillingham <gillingham.sam@gmail.com>
2016-01-28 09:31:26 +1000

    new dll build of lastools

--- lastools/bld.bat
+++ lastools/bld.bat
@@ -1,13 +1,22 @@
 
 REM Supplied VC workspaces don't work for me
-REM Martin doesn't have EXPORTS so can't (easily) make this into a DLL
 nmake -f makefile.vc
 if errorlevel 1 exit 1
 
 mkdir %LIBRARY_LIB%
 copy laslib.lib %LIBRARY_LIB%
+if errorlevel 1 exit 1
+
 mkdir %LIBRARY_BIN%
+copy laslib.dll %LIBRARY_BIN%
+if errorlevel 1 exit 1
+
 copy *.exe %LIBRARY_BIN%
+if errorlevel 1 exit 1
+
 mkdir %LIBRARY_INC%
 copy LASlib\inc\*.* %LIBRARY_INC%
+if errorlevel 1 exit 1
+
 copy LASzip\src\*.hpp %LIBRARY_INC%
+if errorlevel 1 exit 1
