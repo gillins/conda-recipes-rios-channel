eff8897d53da4d89f32c72991d1d1303f43cfdce
Sam Gillingham <gillingham.sam@gmail.com>
2016-04-08 11:24:36 +1000

    updates to lastools and pylidar receipes

--- pylidar/bld.bat
+++ pylidar/bld.bat
@@ -1,7 +1,23 @@
 
-REM todo: check for different versions of VC
-set "RIVLIB_ROOT=c:\dev\rivlib-2_1_1-x86_64-windows-vc100"
-set "RIWAVELIB_ROOT=c:\dev\riwavelib_wfm-2_0_1-x86_64-windows-vc100"
+:: Work out MSVC_VER - needed for build process.
+:: Currently guess from Python version 
+if "%CONDA_PY%" == "27" (
+    set MSVC_VER=90
+)
+if "%CONDA_PY%" == "34" (
+    set MSVC_VER=100
+) 
+if "%CONDA_PY%" == "35" (
+    set MSVC_VER=140
+) 
+
+IF "%MSVC_VER%"=="" (
+    echo "Python version not supported. Please update bld.bat"
+    exit 1
+)
+
+set "RIVLIB_ROOT=c:\dev\rivlib-2_1_1-x86_64-windows-vc%MSVC_VER%"
+set "RIWAVELIB_ROOT=c:\dev\riwavelib_wfm-2_0_1-x86_64-windows-vc%MSVC_VER%"
 
 if not exist %RIVLIB_ROOT% (set "RIVLIB_ROOT=")
 if not exist %RIWAVELIB_ROOT% (set "RIWAVELIB_ROOT=")
