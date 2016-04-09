9385df752a64f1cdcb8b37d23f49ef86e745805a
Dan Clewley <daniel.clewley@gmail.com>
2016-04-09 17:03:51 +0100

    Changed find command for LAStools on OS X -executable flag isn't available.

--- lastools/build.sh
+++ lastools/build.sh
@@ -8,4 +8,11 @@ mkdir -p $PREFIX/include
 cp LASlib/inc/* $PREFIX/include
 cp LASzip/src/*.hpp $PREFIX/include
 mkdir -p $PREFIX/bin
-find src -executable -type f -exec cp {} $PREFIX/bin \;
+if [ `uname` == Darwin ]; then
+    # -executable flag isn't available on find for OS X so
+    # check for user execute.
+    find src -perm +u=x -type f -exec cp {} $PREFIX/bin \;
+else
+    find src -executable -type f -exec cp {} $PREFIX/bin \;
+fi
+
