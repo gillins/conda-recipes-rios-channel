f8d76ceb03067b4b77e7777f601bd332b8538cce
Sam Gillingham <gillingham.sam@gmail.com>
2016-05-18 16:31:52 +1000

    update to latest version of Riegl libs

--- pylidar/build.sh
+++ pylidar/build.sh
@@ -2,8 +2,8 @@
 
 # Test to see if the Riegl libs are installed under the users
 # home directory and export the appropriate env vars if so.
-RIVLIB_ROOT=~/rivlib-2_1_0-x86_64-linux-gcc44
-RIWAVELIB_ROOT=~/riwavelib_wfm-2_1_0-x86_64-linux-gcc44
+RIVLIB_ROOT=~/rivlib-2_3_0-x86_64-linux-gcc44
+RIWAVELIB_ROOT=~/riwavelib_wfm-2_3_0-x86_64-linux-gcc44
 
 if [ -d "$RIVLIB_ROOT" ]; then
     export RIVLIB_ROOT
@@ -17,4 +17,4 @@ fi
 # this should always be installed - part of the prereqs
 export LASTOOLS_ROOT=$PREFIX
 
-python setup.py install --prefix=$PREFIX
+$PYTHON setup.py install --prefix=$PREFIX
