8968ff445893e1302e152833c7870f77167e463b
Daniel Clewley <daniel.clewley@gmail.com>
2016-09-10 15:13:44 +0100

    Added SPDLib recipes.

---
+++ spdlib/build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+source activate "${CONDA_DEFAULT_ENV}"
+
+if [ `uname` == Darwin ]; then
+    export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib
+    cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
+    -D HDF5_INCLUDE_DIR=$PREFIX/include \
+    -D HDF5_LIB_PATH=$PREFIX/lib \
+    -D LIBLAS_INCLUDE_DIR=$PREFIX/include \
+    -D LIBLAS_LIB_PATH=$PREFIX/lib \
+    -D GSL_INCLUDE_DIR=$PREFIX/include \
+    -D GSL_LIB_PATH=$PREFIX/lib \
+    -D CGAL_INCLUDE_DIR=$PREFIX/include \
+    -D CGAL_LIB_PATH=$PREFIX/lib \
+    -D BOOST_INCLUDE_DIR=$PREFIX/include \
+    -D BOOST_LIB_PATH=$PREFIX/lib \
+    -D GDAL_INCLUDE_DIR=$PREFIX/include \
+    -D GDAL_LIB_PATH=$PREFIX/lib \
+    -D XERCESC_INCLUDE_DIR=$PREFIX/include \
+    -D XERCESC_LIB_PATH=$PREFIX/lib \
+    -D GMP_INCLUDE_DIR=$PREFIX/include \
+    -D GMP_LIB_PATH=$PREFIX/lib \
+    -D MPFR_INCLUDE_DIR=$PREFIX/include \
+    -D MPFR_LIB_PATH=$PREFIX/lib \
+    -D CMAKE_VERBOSE_MAKEFILE=ON \
+    .
+else
+    export LD_LIBRARY_PATH=$PREFIX/lib
+    cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
+    -D HDF5_INCLUDE_DIR=$PREFIX/include \
+    -D HDF5_LIB_PATH=$PREFIX/lib \
+    -D LIBLAS_INCLUDE_DIR=$PREFIX/include \
+    -D LIBLAS_LIB_PATH=$PREFIX/lib \
+    -D GSL_INCLUDE_DIR=$PREFIX/include \
+    -D GSL_LIB_PATH=$PREFIX/lib \
+    -D CGAL_INCLUDE_DIR=$PREFIX/include \
+    -D CGAL_LIB_PATH=$PREFIX/lib \
+    -D BOOST_INCLUDE_DIR=$PREFIX/include \
+    -D BOOST_LIB_PATH=$PREFIX/lib \
+    -D GDAL_INCLUDE_DIR=$PREFIX/include \
+    -D GDAL_LIB_PATH=$PREFIX/lib \
+    -D XERCESC_INCLUDE_DIR=$PREFIX/include \
+    -D XERCESC_LIB_PATH=$PREFIX/lib \
+    -D GMP_INCLUDE_DIR=$PREFIX/include \
+    -D GMP_LIB_PATH=$PREFIX/lib \
+    -D MPFR_INCLUDE_DIR=$PREFIX/include \
+    -D MPFR_LIB_PATH=$PREFIX/lib \
+    -D CMAKE_VERBOSE_MAKEFILE=ON \
+    .
+fi
+
+make
+make install
