b2657e5718228e14c605e49e2d5baf3684fa8735
Sam Gillingham <gillingham.sam@gmail.com>
2017-08-24 16:18:09 +1000

    updated pylidar recipe

--- pylidar/bld.bat
+++ pylidar/bld.bat
@@ -1,23 +1,5 @@
 
-:: Work out MSVC_VER - needed for build process.
-:: Currently guess from Python version 
-if "%CONDA_PY%" == "27" (
-    set MSVC_VER=90
-)
-if "%CONDA_PY%" == "34" (
-    set MSVC_VER=100
-) 
-if "%CONDA_PY%" == "35" (
-    set MSVC_VER=140
-) 
-if "%CONDA_PY%" == "36" (
-    set MSVC_VER=140
-) 
-
-IF "%MSVC_VER%"=="" (
-    echo "Python version not supported. Please update bld.bat"
-    exit 1
-)
+set MSVC_VER=%VS_MAJOR%0
 
 set "RIVLIB_ROOT=c:\dev\rivlib-2_3_0-x86_64-windows-vc%MSVC_VER%"
 set "RIWAVELIB_ROOT=c:\dev\riwavelib_wfm-2_3_0-x86_64-windows-vc%MSVC_VER%"
@@ -26,7 +8,9 @@ if not exist %RIVLIB_ROOT% (set "RIVLIB_ROOT=")
 if not exist %RIWAVELIB_ROOT% (set "RIWAVELIB_ROOT=")
 
 set LASTOOLS_ROOT=%LIBRARY_PREFIX%
+set PULSEWAVES_ROOT=%LIBRARY_PREFIX%
 set ZLIB_ROOT=%LIBRARY_PREFIX%
+set GDAL_HOME=%LIBRARY_PREFIX%
 
 REM use Python entry points instead of our programs in bin
 set PYLIDAR_NOCMDLINE=1
--- pylidar/build.sh
+++ pylidar/build.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# FIXME: This is a hack to make sure the environment is activated.
-# The reason this is required is due to the conda-build issue
-# mentioned below.
-#
-# https://github.com/conda/conda-build/issues/910
-#
-source activate "${CONDA_DEFAULT_ENV}"
-
 # Test to see if the Riegl libs are installed under the users
 # home directory and export the appropriate env vars if so.
 RIVLIB_ROOT=~/rivlib-2_3_0-x86_64-linux-gcc44
@@ -26,9 +18,12 @@ fi
 export LASTOOLS_ROOT=$PREFIX
 
 # this should always be installed - part of the prereqs
+export PULSEWAVES_ROOT=$PREFIX
+
+# this should always be installed - part of the prereqs
 export ZLIB_ROOT=$PREFIX
 
 # use Python entry points instead of our programs in bin
 export PYLIDAR_NOCMDLINE=1
 
-$PYTHON setup.py install --prefix=$PREFIX
+$PYTHON setup.py install
