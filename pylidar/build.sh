4bf013ad01ff61fe680a9c2c2e8462375a7670dc
Sam Gillingham <gillingham.sam@gmail.com>
2020-07-01 15:23:16 +1000

    Fix Riegl paths

--- pylidar/build.sh
+++ pylidar/build.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-# Test to see if the Riegl libs are installed under the users
-# home directory and export the appropriate env vars if so.
-RIVLIB_ROOT=~/rivlib-2_3_0-x86_64-linux-gcc44
-RIWAVELIB_ROOT=~/riwavelib_wfm-2_3_0-x86_64-linux-gcc44
+# Test to see if the Riegl libs are installed under /opt
+# and export the appropriate env vars if so.
+RIVLIB_ROOT=/opt/riegl/rivlib/rivlib-2_5_6-x86_64-linux-gcc55
+RIWAVELIB_ROOT=/opt/riegl/riwavelib/riwavelib_wfm-2_5_7-x86_64-linux-gcc55
+RDBLIB_ROOT=/opt/riegl/rdblib/rdblib-2.2.1-x86_64-linux
 
 if [ -d "$RIVLIB_ROOT" ]; then
     export RIVLIB_ROOT
-    # needed on older versions of g++
-    export PYLIDAR_CXX_FLAGS="-std=c++0x"
 fi
 if [ -d "$RIWAVELIB_ROOT" ]; then
     export RIWAVELIB_ROOT
+fi
+if [ -d "$RDBLIB_ROOT" ]; then
+    export RDBLIB_ROOT
 fi
 
 # this should always be installed - part of the prereqs
