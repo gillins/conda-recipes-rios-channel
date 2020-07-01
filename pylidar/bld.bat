8ebf5ea84cf0bc70d3b1f523eb913c1f33ef92a4
Sam Gillingham <gillingham.sam@gmail.com>
2020-07-01 14:13:30 +1000

    pylidar 0.4.3

--- pylidar/bld.bat
+++ pylidar/bld.bat
@@ -5,8 +5,8 @@ REM %VS_MAJOR% is set to 15, but we are actually building with the 14 toolset
 REM %vc% seems to be set to the right value.
 set MSVC_VER=%vc%0
 
-set "RIVLIB_ROOT=c:\dev\rivlib-2_3_0-x86_64-windows-vc%MSVC_VER%"
-set "RIWAVELIB_ROOT=c:\dev\riwavelib_wfm-2_3_0-x86_64-windows-vc%MSVC_VER%"
+set "RIVLIB_ROOT=c:\dev\rivlib-2_5_7-x86_64-windows-vc%MSVC_VER%"
+set "RIWAVELIB_ROOT=c:\dev\riwavelib_wfm-2_5_7-x86_64-windows-vc%MSVC_VER%"
 set "RDBLIB_ROOT=C:\dev\rdblib-2.2.1-x86_64-windows"
 
 if not exist %RIVLIB_ROOT% (set "RIVLIB_ROOT=")
--- pylidar/run_test.bat
+++ pylidar/run_test.bat
@@ -1,8 +1,7 @@
 
-exit 0
 IF "%PYLIDAR_TESTDATA%"=="" (
     echo "Set the PYLIDAR_TESTDATA environment variable to location of latest testdata_X.tar.gz"
-    echo "from https://bitbucket.org/chchrsc/pylidar/downloads"
+    echo "from https://github.com/ubarsc/pylidar/wiki/Testdata"
     echo "to run test as part of build"
     exit 0
 ) ELSE (
