f4d4452061c612cadd8770192ccf2faf75f77767
Sam Gillingham <gillingham.sam@gmail.com>
2020-08-20 09:57:58 +1000

    update rdblib

--- pylidar/bld.bat
+++ pylidar/bld.bat
@@ -1,13 +1,11 @@
 
-hg up rdb
-
 REM %VS_MAJOR% is set to 15, but we are actually building with the 14 toolset
 REM %vc% seems to be set to the right value.
 set MSVC_VER=%vc%0
 
 set "RIVLIB_ROOT=c:\dev\rivlib-2_5_7-x86_64-windows-vc%MSVC_VER%"
 set "RIWAVELIB_ROOT=c:\dev\riwavelib_wfm-2_5_7-x86_64-windows-vc%MSVC_VER%"
-set "RDBLIB_ROOT=C:\dev\rdblib-2.2.1-x86_64-windows"
+set "RDBLIB_ROOT=C:\dev\rdblib-2.2.3-x86_64-windows"
 
 if not exist %RIVLIB_ROOT% (set "RIVLIB_ROOT=")
 if not exist %RIWAVELIB_ROOT% (set "RIWAVELIB_ROOT=")
